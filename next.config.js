96c9c5237dd846bc5746a2293be1f2d9dbb4cb72
t7h8qhmb2r-alt <t7h8qhmb2r@privaterelay.appleid.com>
2026-04-20 00:27:21 +0900

    Delete next.config.js

--- next.config.js
+++
@@ -1,3 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = { images: { unoptimized: true, remotePatterns: [{ protocol: 'https', hostname: 'imgfp.hotp.jp' }] } };
-module.exports = nextConfig;
