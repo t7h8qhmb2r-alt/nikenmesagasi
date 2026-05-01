509dadcbea13d1fcc2aeca430f34eb85936a9a79
t7h8qhmb2r-alt <t7h8qhmb2r@privaterelay.appleid.com>
2026-05-01 10:58:22 +0900

    Delete app directory

--- app/layout.tsx
+++
@@ -1,4 +0,0 @@
-import "./globals.css";
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (<html lang="ja"><body className="antialiased">{children}</body></html>);
-}
